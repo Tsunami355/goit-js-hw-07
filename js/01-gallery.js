f49b771d7c087f076b2168e8c6161d12c6e8dc3d
Tsunami355 <136748723+Tsunami355@users.noreply.github.com>
2023-10-28 18:34:42 +0300

    List image

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listImages = document.querySelector(".gallery");
+console.log(listImages);
+
+const imagesMarkup = creatImagesListMarkup(galleryItems);
+
+listImages.insertAdjacentHTML('beforeend', imagesMarkup);
+listImages.addEventListener('click', onCardsContainerClick);
+
+function creatImagesListMarkup(images) {
+    return images
+        .map(({ original, preview, description }) => {
+            return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+        })
+        .join(''); 
+}
