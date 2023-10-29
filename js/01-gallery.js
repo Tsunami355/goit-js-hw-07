b9039c253cc8edbaf64d44c3eaefc55e6db7fa88
Tsunami355 <136748723+Tsunami355@users.noreply.github.com>
2023-10-29 17:57:10 +0200

    hw-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-
 const listImages = document.querySelector(".gallery");
-console.log(listImages);
 
-const imagesMarkup = creatImagesListMarkup(galleryItems);
-
-listImages.insertAdjacentHTML('beforeend', imagesMarkup);
-
-
-function creatImagesListMarkup(images) {
-    return images
-        .map(({ original, preview, description }) => {
+function creatImagesList(images) {
+    const markup = images.map(({ original, preview, description }) => {
             return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -23,17 +14,20 @@ function creatImagesListMarkup(images) {
       alt="${description}"
     />
   </a>
-</li>`;
+</li>`
         })
         .join(''); 
+    
+    listImages.innerHTML = markup;
 }
+
 
 listImages.addEventListener('click', onClick);
 
-function onClick(evn) {
-    evn.preventDefault();
-    const target = evn.target;
-
+function onClick(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+console.log(evt.target)
     if (target.nodeName !== 'IMG') {
         return;
     }
@@ -48,9 +42,3 @@ function onClick(evn) {
 
 console.log(galleryItems);
 
-// const instance = basicLightbox.create(`
-//     <h1>Dynamic Content</h1>
-//     <p>You can set the content of the lightbox with JS.</p>
-// `)
-// console.log(instance);
-// instance.show();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listImages = document.querySelector(".gallery");
+
+
+function creatImagesList(images) {
+    const markup = images.map(({ original, preview, description }) => {
+            return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+        })
+        .join(''); 
+    
+    listImages.innerHTML = markup;
+}
+
+creatImagesList(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
 
 console.log(galleryItems);
